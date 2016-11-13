f64f962fd5876635cd824dbc1319e3532ffc59fc
Pawit Pornkitprasan <p.pawit@gmail.com>
2016-11-13 11:06:46 +0700

    Use mongodb for phone

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var Botkit = require('botkit')
 var Witbot = require('witbot')
+var MongoClient = require('mongodb').MongoClient
 var _ = require('underscore')
 
 // setting data 
@@ -7,22 +8,29 @@ var menus = require('./data/menus');
 
 var slackToken = process.env.SLACK_TOKEN
 var witToken = process.env.WIT_TOKEN
-
-
+var mongoUrl = process.env.MONGO_URL
 var _debug = process.env.DEBUG
+
 var witbot = Witbot(process.env.WIT_TOKEN)
 var controller = Botkit.slackbot({ debug: false })
-var phonebook = require('./data/phone.json');
 if(_debug)
 {
     console.log("SlackToken:" + slackToken)
     console.log("WitToken:" + witToken)
 }
 
-controller.spawn({ token: slackToken }).startRTM(function (err, bot, payload) {
-  if (err) throw new Error('Error connecting to Slack: ', err)
-  console.log('Connected to Slack')
-})
+var db;
+
+MongoClient.connect(mongoUrl, function(err, dbConn) {
+  if (err) throw new Error('Error connecting to MongoDB: ', err)
+  console.log("Connected successfully to MongoDB");
+  db = dbConn;
+
+  controller.spawn({ token: slackToken }).startRTM(function (err, bot, payload) {
+    if (err) throw new Error('Error connecting to Slack: ', err)
+    console.log('Connected to Slack')
+  })
+});
 
 // wire up DMs and direct mentions to wit.ai
 controller.hears('.*', 'direct_message,direct_mention', function (bot, message) {
@@ -43,16 +51,18 @@ controller.hears('.*', 'direct_message,direct_mention', function (bot, message) 
 
       var intent = (outcome.entities.intent == null) ? '' : outcome.entities.intent[0].value;
       if (intent == 'phone' && outcome.entities.name != null) {
-        var lookup = _.findWhere(phonebook,{"name":outcome.entities.name[0].value});
-        if(lookup)
-        {
-          bot.reply(message, 'Someone told me that '+ outcome.entities.name[0].value + '\'s phone number is ' + lookup.phone );
-        }
-        else
-        {          
-          bot.reply(message, 'Ask ' + outcome.entities.name[0].value + ' himself!');
-        }
-
+        var searchName = outcome.entities.name[0].value;
+        console.log('Searching for ' + searchName);
+        db.collection('phone').find({nickname: searchName}).toArray(function(err, docs) {
+          if (err) {
+            console.log(err);
+            bot.reply(message, 'My brain has dry ice.')
+          } else if (docs.length > 0) {
+            bot.reply(message, 'Someone told me that '+ docs[0].nickname + '\'s phone number is ' + docs[0].phone );
+          } else {
+            bot.reply(message, 'Ask ' + searchName + ' himself!');
+          }
+        });
       } else if (intent == 'call') {
         bot.reply(message, "I'm not a phone!");
       } else if (intent == 'lunch') {
