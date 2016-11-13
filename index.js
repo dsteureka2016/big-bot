cc3a147773cfef1417159eaa96f7ddc19103d181
pwongwasinkul <patwonggtr@gmail.com>
2016-11-13 17:16:24 +0700

    change to use bangkok time and prevent null user.

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ controller.hears('.*', 'direct_message,direct_mention,mention', function (bot, m
         var greetingPhrase = "Hello";
         if (message.ts) {
           var date = new Date(message.ts * 1000);
-          var time = date.getHours();
+          var time = (date.getHours() + 7) % 24;
           if (time >= 0 && time <= 12) {
             greetingPhrase = "Good morning";
           } else if (time >= 12 && time <= 16) {
@@ -67,7 +67,11 @@ controller.hears('.*', 'direct_message,direct_mention,mention', function (bot, m
             greetingPhrase = "Good night";
           }
         }
-        bot.reply(message, greetingPhrase + " <@" + message.user + "|user>.");
+        if (message.user) {
+          bot.reply(message, greetingPhrase + " user.");  
+        } else {
+          bot.reply(message, greetingPhrase + " <@" + message.user + ">.");
+        }
       } else {
         bot.reply(message, msgUtil.idontunderstand());
       }
