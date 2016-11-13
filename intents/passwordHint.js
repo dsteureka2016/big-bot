14f96d9bea46c2169e6f0e665e779fd1fd7f7c76
Isara Rungvitayakul <isaru_66@hotmail.com>
2016-11-13 15:57:25 +0700

    trim system

--- intents/passwordHint.js
+++ intents/passwordHint.js
@@ -1,20 +1,23 @@
 exports.intent_name = 'password_hint'
 
 exports.respond = function(bot, message, db, entities) {
-        var searchSystem = entities.system[0].value;
-        console.log(' ' + searchSystem);
+        var searchSystem = entities.system[0].value.trim();
+        if(searchSystem)
+        {
+            console.log(' ' + searchSystem);
 
-        db.collection('passwordHintSystem').find({ $or: [{ name: searchSystem }] }).toArray(function(err, docs) {
-                if (err) {
-                    console.log(err);                    
-                    bot.reply(message, 'My brain has dry ice.')
-                } else if (docs.length > 0) {
-                    for (var i = 0; i < docs.length; i++) {
-                        bot.reply(message, docs[i].hint );
+            db.collection('passwordHintSystem').find({ $or: [{ name: searchSystem }] }).toArray(function(err, docs) {
+                    if (err) {
+                        console.log(err);                    
+                        bot.reply(message, 'My brain has dry ice.')
+                    } else if (docs.length > 0) {
+                        for (var i = 0; i < docs.length; i++) {
+                            bot.reply(message, docs[i].hint );
+                        }
+                    } else {
+                        bot.reply(message, 'Sorry, I have no idea about that')
                     }
-                } else {
-                    bot.reply(message, 'Sorry, I have no idea about that')
                 }
-            }
-        )
+            )
       }
+}
