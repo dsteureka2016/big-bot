542d416582c5272ed1554d93be0202492693d0fa
dt85452 <nitisak.mooltreesri@dstworldwideservices.com>
2016-11-13 16:42:14 +0700

    Add new file

---
+++ intents/remind.js
@@ -0,0 +1,10 @@
+exports.intent_name = 'remind'
+
+exports.respond = function(bot, message, db, entities) {
+  var memberName = entities.members[0].value;
+  var className = entities.class[0].value;
+  var dueDate = entities.duedate[0].value;
+  console.log('Searching for ' + memberName);
+
+ 
+}
